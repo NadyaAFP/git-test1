68f33a89b50c29704ff8c938546d14e0e1d02559
nadyaafp <firstiany.putri@gmail.com>
2020-10-05 10:36:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale1.0">
+ </tittle> Latihan GIT remote </tille>
  </head>
  <body>
     <h1> Hellow World </h1>
     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quad optio provident! Sit beatae, expedita quod recusandae aut voluptas dicta rej <\p>
-    </body>
-    </html>
+    <ul>
+    <li> List1 </li>
+    <li> List2 </li>
+    </ul>
+</body>
+</html>
