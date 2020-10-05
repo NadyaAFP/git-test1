57f726837f454b991bfcacc3eb8fb00b724f5dc7
nadyaafp <firstiany.putri@gmail.com>
2020-10-05 10:02:49 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lange="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale1.0">
+ </head>
+ <body>
+    <h1> Hellow World </h1>
+    </body>
+    </html>
